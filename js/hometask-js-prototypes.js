3ca010d046e02b27f7e163923954181fa41730ec
DiMa <dbelousov13@gmail.com>
2021-05-26 10:16:55 +0300

    Final commit hometas js.protoypes

--- js/hometask-js-prototypes.js
+++ js/hometask-js-prototypes.js
@@ -9,6 +9,13 @@ function User(name, surname, age, isMale, email, isSubscribed) {
   this.isSubscribed = isSubscribed;
 }
 
+const userProto = new User();
+userProto.getFullName = function () {
+  console.log(`${this.firstName} ${this.lastName}`);
+};
+
+User.prototype = userProto;
+
 const users = [];
 
 for (let i = 0; i < 100; i++) {
@@ -23,46 +30,71 @@ for (let i = 0; i < 100; i++) {
   users.push(user);
 }
 
-// Получить только женщин
-const womanUsers = users.filter(isWoman);
+// // Получить только женщин
+// const womanUsers = users.filter(isWoman);
 
-function isWoman(user) {
-  return !user.isMale;
-}
+// function isWoman(user) {
+//   return !user.isMale;
+// }
+// console.log(`Получить только женщин`);
+// console.table(womanUsers);
 
-console.table(womanUsers);
+// // Получить совершеннолетних неподписанных мужчин
+// const adultUnsubMen = users.filter(isAdultUnsubMan);
 
-// Получить совершеннолетних неподписанных мужчин
-const adultUnsubMen = users.filter(isAdultUnsubMan);
+// function isAdultUnsubMan(user) {
+//   return user.isMale && user.age >= 18 && !user.isSubscribed;
+// }
 
-function isAdultUnsubMan(user) {
-  return user.isMale && user.age >= 18 && !user.isSubscribed;
-}
+// console.table(adultUnsubMen);
 
-console.table(adultUnsubMen);
+// // Получить массив имейлов пользователей
+// const userEmails = users.map(getEmail);
 
-// Получить массив имейлов пользователей
-const userEmails = users.map(getEmail);
+// function getEmail(user) {
+//   return user.email;
+// }
 
-function getEmail(user) {
-  return user.email;
-}
+// console.table(userEmails);
 
-console.table(userEmails);
-
-// Получить массив имейлов только женщин
-const womanEmails = users.filter(isWoman).map(getEmail);
-console.table(womanEmails);
+// // Получить массив имейлов только женщин
+// const womanEmails = users.filter(isWoman).map(getEmail);
+// console.table(womanEmails);
 
 // 1.1 Для примера с последней части занятия (https://github.com/pecourses/js-intro/blob/main/js/users.js) прописать метод getFullName() (возвращает строку с полным именем) для юзера. Общую логику (т.е. указанный метод) вынести в прототип.
+// метод -  строка 12
+console.log(
+  "1.1 Для примера с последней части занятия (https://github.com/pecourses/js-intro/blob/main/js/users.js) прописать метод getFullName() (возвращает строку с полным именем) для юзера. Общую логику (т.е. указанный метод) вынести в прототип."
+);
+users.forEach((user) => user.getFullName());
 
 //     1.2 Получить массив полных имен лиц женского пола школьного возраста(6 - 18 лет).
 const schoolWoman = users.filter(school);
 function school(user) {
   return !user.isMale && user.age >= 6 && user.age <= 18;
 }
+console.log(
+  `1.2 Получить массив полных имен лиц женского пола школьного возраста(6 - 18 лет).`
+);
 console.table(schoolWoman);
+schoolWoman.forEach((user) => user.getFullName());
 
 // 1.3 Проверить, есть ли среди пользователей пользователь с email`ом useremail99@gmail.com
+const includesEmail = users.some(mails);
+function mails(user) {
+  return user.email === "useremail99@gmail.com";
+}
+console.log(
+  `1.3. Проверить, есть ли среди пользователей пользователь с email'ом useremail99@gmail.com:`
+);
+console.log(`${includesEmail}`);
 
 // 1.4 Проверить, все ли пользователи подписаны (subscribed).
+
+const isAllSubscribed = users.every(subscribed);
+function subscribed(user) {
+  return user.isSubscribed;
+}
+
+console.log(`1.4. Проверить, все ли пользователи подписаны (subscribed):`);
+console.log(`${isAllSubscribed}`);
